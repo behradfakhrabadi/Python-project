bd40f83e85580a2e6037fcae347871c360aa3c42
behradfakhrabadi <115945313+behradfakhrabadi@users.noreply.github.com>
2022-10-23 09:28:09 +0330

    Create EX1_4.py

---
+++ EX1/EX1_4.py
@@ -0,0 +1,19 @@
+def EX1_4(day):
+  if day==1:
+    return "Monday"
+  elif day==2:
+    return "Tuesday"
+  elif day==3:
+    return "Wednesday"
+  elif day==4:
+    return "Thursday"
+  elif day==5:
+    return "Friday"
+  elif day==6:
+    return "Saturday"
+  elif day==7:
+    return "Sunday"
+  else:
+    return"error"
+  
+    
