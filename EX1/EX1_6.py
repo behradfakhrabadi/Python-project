c277fd54ef356a49d5fbbc474bb115fca3ac9d98
behradfakhrabadi <115945313+behradfakhrabadi@users.noreply.github.com>
2022-10-23 09:47:08 +0330

    Create EX1_6.py

---
+++ EX1/EX1_6.py
@@ -0,0 +1,24 @@
+def EX1_6(roman):
+  if roman==1:
+    return "I"
+  elif roman==2:
+    return "II"
+  elif roman==3:
+    return "III"
+  elif roman==4:
+    return "IV"
+  elif roman==5:
+    return "V"
+  elif roman==6:
+    return "VI"
+  elif roman==7:
+    return "VII"
+  elif roman==8:
+    return "VIII"
+  elif roman==9:
+    return "IX"
+  elif roman==10:
+    return "X"
+  else:
+    return "error"
+  
