ecd62c7a24792e5839c4e3ee724b07a41872b468
behradfakhrabadi <115945313+behradfakhrabadi@users.noreply.github.com>
2022-10-26 07:32:06 +0330

    Update EX1_5.py

--- EX1/EX1_5.py
+++ EX1/EX1_5.py
@@ -1,4 +1,10 @@
 def EX1_5(age):
+  '''
+  classify the age group of people by taking their age
+  ex:
+  >>>EX1_5(12):
+  child
+  '''
   if age<=1 and age>0:
     return "infant"
   if age>1 and age<13:
