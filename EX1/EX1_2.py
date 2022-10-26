e23c9300eb73b3d73d3bdf90808f7f8be2158af0
behradfakhrabadi <115945313+behradfakhrabadi@users.noreply.github.com>
2022-10-26 07:26:26 +0330

    Update EX1_2.py

--- EX1/EX1_2.py
+++ EX1/EX1_2.py
@@ -1,4 +1,10 @@
 def EX1_2(height, base):
+  '''
+  we give height and base of triangle and python calculate the area of it
+  ex:
+  >>>EX1_2(7, 8):
+  28
+  '''
   if height>0 and base>0:
     area= (base*height)/2
     return area
